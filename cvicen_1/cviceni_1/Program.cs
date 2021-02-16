1ebeb51e73b2d83a0a3ea7ce2ba2ee3ede541f8b
vaclav.skorpil <skorpil.vaclav0@gmail.com>
2021-02-16 17:59:19 +0100

    initial commit

---
+++ cvicen_1/cviceni_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace cviceni_1
+{
+    public class Program
+    {
+        public string Myproperty { get; set; }
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
